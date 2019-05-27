27066bd895fded493fe9d2786421bf4b4b4b3011
olesy <olesya.mogil@gmail.com>
2019-05-27 20:12:13 +0300

    'test'

--- remindLesson.js
+++ remindLesson.js
@@ -1,13 +1,9 @@
+const config = require('./config.js');
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
 
 express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
     .get('/', (req, res) => {
 
         res.end(`Olololo`)
     })
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .listen(config.APP_PORT, () => console.log(`Listening on ${ PORT }`))
