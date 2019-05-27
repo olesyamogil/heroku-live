c0ea2c40a8c653a89110c2f6c406ba8b3ca9f400
olesy <olesya.mogil@gmail.com>
2019-05-27 20:13:55 +0300

    'test'

--- remindLesson.js
+++ remindLesson.js
@@ -1,9 +1,4 @@
 const config = require('./config.js');
-const express = require('express')
-
-express()
-    .get('/', (req, res) => {
-
-        res.end(`Olololo`)
-    })
-    .listen(config.APP_PORT, () => console.log(`Listening on ${ PORT }`))
+module.exports = (req, res) => {
+    res.end('dddddd');
+}
