a42a9e298344018072b37d10e445ea9f28f7d933
olesy <olesya.mogil@gmail.com>
2019-05-29 22:20:04 +0300

    'test'

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const config = require('./config.js');
 
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(config.TELEGRAM_TOKEN);
-// bot.setWebHook(`${url}/bot${config.TELEGRAM_TOKEN}`);
 const easycron = require("easy-cron")({ token: config.EASY_CRON_TOKEN });
+const phantom = require('phantom');
 
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
@@ -40,9 +40,14 @@ function parseSchedule(userId, chatId) {
                     if (secondColumn.innerHTML
                         && firstColumnChildNodes.length
                         && firstColumnChildNodes.length === 3) {
-                        const timeTokens = firstColumnChildNodes[2].textContent.split(':');
-                        const classStartHour = timeTokens[0];
-                        const classStartMinute = timeTokens[1];
+                        const TIME_COLUMN_INDEX = 2;
+                        const timeTokens = firstColumnChildNodes[TIME_COLUMN_INDEX].textContent.split(':');
+
+                        const HOURS_TOKEN_INDEX = 0;
+                        const classStartHour = timeTokens[HOURS_TOKEN_INDEX];
+
+                        const MINUTES_TOKEN_INDEX = 1;
+                        const classStartMinute = timeTokens[MINUTES_TOKEN_INDEX];
 
                         const classDescription = secondColumnArray.reduce(
                             (accumulator, currentValue) => {
@@ -74,7 +79,95 @@ function parseSchedule(userId, chatId) {
 
         });
 }
+function sendForm (){
+    JSDOM.fromURL('http://rozklad.kpi.ua/Schedules/ScheduleGroupSelection.aspx')
+        .then((dom) => {
+            const form = dom.window.document.querySelectorAll('form input');
+            const form1 = dom.window.document.querySelectorAll('form');
+            let formData = {};
+            console.log(form1);
+            console.log("===========================");
+            let formArray = Array.from(form);
+            formArray.forEach(function(item, index){
+                formData[item.getAttribute("name")]=item.value;
+                console.log(index + item.getAttribute("name") + '------' + item.value + '\n');
+            });
+            formData["ctl00$MainContent$ctl00$txtboxGroup"] = "ІС-з61";
+            let request = require("request"),
+                options = {
+                    url: 'http://rozklad.kpi.ua/Schedules/ScheduleGroupSelection.aspx',
+                    timeout: 2000,
+                    followAllRedirects: true,
+                    method: 'POST',
+                    formData: formData
+                };
+            request.post( options, function(error, response, body) {
+                console.log( body );
+                const scheduleDOM = new JSDOM(body);
+                const rows = scheduleDOM.window.document.getElementById('ctl00_MainContent_FirstScheduleTable').getElementsByTagName('tr');
+                let currentDay;
+                let currentMonth;
+                for (let rowIndex = 0; rowIndex < rows.length; rowIndex += 1) {
+                    const item = rows[rowIndex];
+                    const firstColumn = item.querySelector('td:nth-child(1)');
+                    const secondColumn = item.querySelector('td:nth-child(2)');
 
+                    // Head line for day
+                    if (!firstColumn.textContent
+                        && secondColumn.textContent
+                        && secondColumn.textContent.match(/[0123]\.[01][0-9]/)) {
+                        const dateTokens = secondColumn.textContent.split('.');
+                        currentDay = parseInt(dateTokens[0]);
+                        currentMonth = parseInt(dateTokens[1]);
+                    } else { // Every single class
+                        const firstColumnChildNodes = firstColumn.childNodes;
+                        const secondColumnChildNodes = secondColumn.childNodes;
+                        const secondColumnArray = Array.from(secondColumnChildNodes);
+
+                        if (secondColumn.innerHTML
+                            && firstColumnChildNodes.length
+                            && firstColumnChildNodes.length === 3) {
+                            const TIME_COLUMN_INDEX = 2;
+                            const timeTokens = firstColumnChildNodes[TIME_COLUMN_INDEX].textContent.split(':');
+
+                            const HOURS_TOKEN_INDEX = 0;
+                            const classStartHour = timeTokens[HOURS_TOKEN_INDEX];
+
+                            const MINUTES_TOKEN_INDEX = 1;
+                            const classStartMinute = timeTokens[MINUTES_TOKEN_INDEX];
+
+                            const classDescription = secondColumnArray.reduce(
+                                (accumulator, currentValue) => {
+                                    return accumulator + currentValue.textContent + '\n'
+                                },
+                                '');
+                            const message = `${currentDay}.${currentMonth} ${classStartHour}:${classStartMinute} \n ${classDescription}`;
+                            bot.sendMessage(userId, message);
+                            easycron.add({
+                                minute: classStartMinute,
+                                hour: classStartHour,
+                                day: currentDay,
+                                month: currentMonth,
+                                url: `https://api.telegram.org/bot${config.TELEGRAM_TOKEN}/sendMessage?chat_id=${chatId}&text=${classDescription}`,
+                                method: 'GET',
+                                headers:{
+                                },
+                                payload: {
+                                }
+                            }).then(function(response) {
+                                console.log("Cron Job Id is " + response.cron_job_id);
+                            }).catch(function(error) {
+                                console.log(error)
+                            });
+
+                        }
+                    }
+                }
+            });
+        });
+
+}
+sendForm();
 bot.onText(/remind (.+) at (.+)/, (msg, match) => {
     const userId = msg.from.id;
     const chatId = msg.chat.id;
@@ -110,23 +203,15 @@ app.use(bodyParser.json());
 
 // Function to handle the root path
 app.post('/sendNow', (req, res) => {
-    console.log("++++");
-    console.log(req.body);
     const PAYLOAD = JSON.parse(req.body.payload);
     bot.sendMessage(PAYLOAD.chat_id, PAYLOAD.text);
 
-
-
-    // Return the articles to the rendering engine
-    res.end('ddddd');
+    res.end('OK');
 });
 // Function to handle the root path
 app.post(`/bot${config.TELEGRAM_TOKEN}`, (req, res) => {
     console.log(req.body);
     bot.processUpdate(req.body);
-    // Access the provided 'page' and 'limt' query parameters
-    // let page = req.query.page;
-    // let limit = req.query.limit;
 
     // Return the articles to the rendering engine
     res.end('ddddd');
--- send.js
+++ send.js
@@ -1,20 +1,84 @@
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 const FormData = require('form-data');
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+//const http = require('http');
+const fetch = require('node-fetch');
 
 function sendForm (){
     JSDOM.fromURL('http://rozklad.kpi.ua/Schedules/ScheduleGroupSelection.aspx')
         .then((dom) => {
-            const form = dom.window.document.getElementsByTagName('form');
-            var formData = new FormData(form);
-            formData.append( 'ctl00$MainContent$ctl00$txtboxGroup', 'ІСз-61');
-            console.log(formData);
-            var request = new XMLHttpRequest();
-            request.open("POST", "ScheduleGroupSelection.aspx");
-            request.send(formData);
-            // console.log(request.response);
+            const form = dom.window.document.querySelectorAll('form input');
+            const form1 = dom.window.document.querySelectorAll('form');
+            let formData = {};
+            console.log(form1);
+            console.log("===========================");
+            let formArray = Array.from(form);
+            formArray.forEach(function(item, index){
+                formData[item.getAttribute("name")]=item.value;
+                console.log(index + item.getAttribute("name") + '------' + item.value + '\n');
+            });
+            formData["ctl00$MainContent$ctl00$txtboxGroup"] = "ІС-з61";
+            let request = require("request"),
+                options = {
+                    url: 'http://rozklad.kpi.ua/Schedules/ScheduleGroupSelection.aspx',
+                    timeout: 2000,
+                    followAllRedirects: true,
+                    method: 'POST',
+                    formData: formData
+                };
+            request.post( options, function(error, response, body) {
+                console.log( body );
+                const dom = new JSDOM(body);
+            });
+
+            // fetch('http://rozklad.kpi.ua/Schedules/ScheduleGroupSelection.aspx',
+            //     { method: 'POST', body: formData, redirect: 'follow',  follow: 20})
+            //     .then(res => res.text())
+            //     .then(body => console.log(body));
+
+            // let form = new FormData();
+            //
+            // form.append('a', 1);
+            //
+            // fetch('http://example.com', { method: 'POST', body: form })
+            //     .then(function(res) {
+            //         return res.json();
+            //     }).then(function(json) {
+            //     console.log(json);
+            // });
+
+            // formData.submit('http://rozklad.kpi.ua/Schedules/ScheduleGroupSelection.aspx', function(err, res) {
+            //     // res – response object (http.IncomingMessage)  //
+            //     let a = res.resume();
+            //     console.log(a.responseText);
+            //     console.log(Object.getOwnPropertyNames(res));
+            //     // console.log(res);
+            //     console.log(a.statusMessage);
+            //     console.log(a.headers);
+            //     console.log(a.responseText);
+            //     console.log(a.response);
+            //     console.log(a.window);
+            //     console.log(a.body);
+            //     console.log(a.content);
+            //     // console.log(a.req);
+            // });
+
         });
 
 }
 sendForm();
+
+
+
+// form.submit('http://example.org/', function(err, res) {
+// //     // res – response object (http.IncomingMessage)  //
+// //     res.resume();
+// // });
+
+// request.post({url:'ScheduleGroupSelection.aspx', formData: form}, function(err, httpResponse, body) {
+//     if (err) {
+//         return console.error('upload failed:', err);
+//     }
+//     console.log('Upload successful!  Server responded with:', body);
+// });
+/*/Schedules/ViewSchedule.aspx?g=607599b2-3369-4bda-8320-803f33aac337*/
